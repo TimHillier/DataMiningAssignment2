8a3826d8cf43ce6b0280637f8ad52a0814d93df1
Tim Hillier <Tim.r.hillier@gmail.com>
2018-11-02 20:40:16 -0700

    Removes elements below the threshold Do I just keep all this in main?

--- Main.py
+++ Main.py
@@ -1,13 +1,24 @@
 #import FileIO
 import sys, os.path
 
+minSupport = -1
+numberOfLines = -1
+
+#dictionary to store the amount:
+CountDictionary = {}
+
+
 
 
 #Main Runner of the code
 def Main():
+    global minSupport
     arguments = GetArguments()
     File = readFile(arguments[1])
-
+    minSupport = (int) (numberOfLines) * ((int) (arguments[2]) / 100)
+    print("Minsup:", minSupport)
+    Trim()
+    print(CountDictionary)
 
 #get and manipulate arguemnts
 def GetArguments():
@@ -16,13 +27,44 @@ def GetArguments():
 
 #Reads files into the code
 def readFile(fileName):
-
+    global numberOfLines
     if(os.path.isfile(fileName)):
+
         file = open(fileName,"r")
-        print(file.read())
+
+        numberOfLines = int(file.readline())
+
+        for i in range (0,numberOfLines):
+            a = file.readline().split()
+            a = a[2:]
+            CheckDictionary(a)
+
+
+        file.close()
         return file
     else:
         print("Not a file Path")
+
+#check the dictionary and if they exist increase counter else ignore
+def CheckDictionary(Elements):
+
+    for i in range (0,len(Elements)):
+        if(not (Elements[i] in CountDictionary)):
+            #if not in the dictionary add it
+            CountDictionary[Elements[i]] = 1
+        else:
+            #if in the dictionary increase by one
+            x = CountDictionary.get(Elements[i]) + 1
+            CountDictionary.update({Elements[i]:x})
+
+#trim the dictionary for elements that are below the minimum support
+def Trim():
+    Delete = []
+    for x,y in CountDictionary.items():
+        if(y < minSupport):
+            Delete.append(x)
+    for x in Delete:
+        CountDictionary.pop(x)
 
 
 Main()
